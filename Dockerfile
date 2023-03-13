8d28a2229689f615f0dcb4ad33afc0194996998a
chenhongwei01 <chenhongwei01@baidu.com>
2023-03-13 11:03:16 +0800

    submit dockerfile code

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM golang:alpine
+RUN mkdir -p /home/goin-mframe
+WORKDIR /home/goin-mframe
+COPY . /home/goin-mframe
+RUN cd /home/goin-mframe
+ENV GO111MODULE=on
+ENV GOPROXY="https://goproxy.cn"
+RUN go mod tidy
+RUN go build .
+ENTRYPOINT ["./gin-gonic"]
--- index.go
+++ index.go
@@ -1,18 +1,25 @@
 package main
 
 import (
+	"io"
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/library"
 )
 
 func main() {
-	r := gin.Default()
+	r := gin.New()
 
 	//加载模板
 	r.LoadHTMLGlob("template/*")
 
 	//初始化路由
 	library.InitAppRouter(r)
+
+	//gin日志
+	logFile, _ := os.OpenFile("log/errors.log", os.O_RDWR|os.O_CREATE, 0755)
+	gin.DefaultWriter = io.MultiWriter(logFile)
 
 	//启动服务
 	app := library.App{}
